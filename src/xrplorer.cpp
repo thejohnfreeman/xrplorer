620edc1b587634f3f4db550a2399608043f3586a
John Freeman <jfreeman08@gmail.com>
2024-05-08 12:23:08 -0500

    Implement REPL

--- src/xrplorer.cpp
+++ src/xrplorer.cpp
@@ -1,8 +1,43 @@
 #include <cstdio>
+#include <cstdlib>
 
 #include <xrplorer/xrplorer.hpp>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
+class LineReader {
+private:
+    char* line_;
+
+public:
+    LineReader() {}
+    LineReader(LineReader const&) = delete;
+    LineReader(LineReader&& rhs) : line_(rhs.line_) { rhs.line_ = nullptr; }
+    LineReader& operator= (LineReader const&) = delete;
+    LineReader& operator= (LineReader&& rhs) {
+        line_ = rhs.line_;
+        rhs.line_ = nullptr;
+        return *this;
+    }
+    ~LineReader() {
+        std::free(line_);
+    }
+
+    char const* readline(char const* prompt) {
+        line_ = ::readline(prompt);
+        return line_;
+    }
+};
+
 int main(int argc, const char** argv) {
-    std::printf("hello, world!\n");
+    LineReader lineReader;
+    while (auto line = lineReader.readline("> ")) {
+        if (!*line) {
+            continue;
+        }
+        ::add_history(line);
+        std::printf("echo: \"%s\"\n", line);
+    }
     return 0;
 }
